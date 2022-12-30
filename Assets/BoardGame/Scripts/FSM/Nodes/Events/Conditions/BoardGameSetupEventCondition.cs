59ee56f0b9b11a1a08c99c9426f3e1424a88a01f
Alan Wills <alawills@googlemail.com>
2022-12-30 09:41:28 +0000

    Working on board setup and setting up current hand event

---
+++ Assets/BoardGame/Scripts/FSM/Nodes/Events/Conditions/BoardGameSetupEventCondition.cs
@@ -0,0 +1,11 @@
+﻿using Celeste.FSM.Nodes.Events.Conditions;
+using System.ComponentModel;
+using WOTR.BoardGame.Events;
+
+namespace WOTR.FSM.Nodes.Events.Conditions
+{
+    [DisplayName("Board Game Setup")]
+    public class BoardGameSetupEventCondition : ParameterizedEventCondition<BoardGameSetupArgs, BoardGameSetupEvent>
+    {
+    }
+}
